e264d5cb5603a40245c99ae1903145bf167f7664
RafaelMendoza <rafael.argentieri@gmail.com>
2019-02-25 23:00:34 -0300

    Criação de projeto simples utilizando TDD com NodeJS

---
+++ test/index.js
@@ -0,0 +1,34 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const somarNumeros = (a,b) => {
+    if(typeof a === "number" && typeof b === "number") return a + b;
+    else return undefined;
+
+};
+
+describe('Somas', () =>{
+    it ('Soma de dois numeros - 2 e 3', (done) =>{
+        const resultado = somarNumeros(2,3);
+        expect(resultado).be.equal(5);
+        done();
+    })
+
+    it ('Soma de dois numeros - -2 e 3', (done) =>{
+        const resultado = somarNumeros(-2,3);
+        expect(resultado).be.equal(1);
+        done();
+    })
+
+    it ('Soma de dois numeros - "teste" e 3', (done) =>{
+        const resultado = somarNumeros("teste",3);
+        expect(resultado).be.equal(undefined);
+        done();
+    })
+
+    it ('Soma de dois numeros - null e 3', (done) =>{
+        const resultado = somarNumeros(null,3);
+        expect(resultado).be.equal(undefined);
+        done();
+    })
+})
